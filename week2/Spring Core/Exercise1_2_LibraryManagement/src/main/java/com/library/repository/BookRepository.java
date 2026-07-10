475ee42e4727e9d34e318e1e06aaa75f789f86b3
khwa04 <04khwahish@gmail.com>
2026-07-10 23:14:02 +0530

    Week 3

---
+++ week2/Spring Core/Exercise1_2_LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,17 @@
+package com.library.repository;
+
+public class BookRepository {
+
+
+    public void display() {
+
+        System.out.println(
+
+                "Book Repository Loaded"
+
+        );
+
+    }
+
+
+}
